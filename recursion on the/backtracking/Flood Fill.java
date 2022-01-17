6d2a70c2cdb831960a37f02f5929d3ee571feb52
Rushikesh Pawar <90256959+rushipawarms@users.noreply.github.com>
2022-01-17 22:15:32 +0530

    Create Flood Fill.java

---
+++ recursion on the/backtracking/Flood Fill.java
@@ -0,0 +1,53 @@
+// 1. You are given a number n, representing the number of rows.
+// 2. You are given a number m, representing the number of columns.
+// 3. You are given n*m numbers, representing elements of 2d array a. The numbers can be 1 or 0 only.
+// 4. You are standing in the top-left corner and have to reach the bottom-right corner. 
+// Only four moves are allowed 't' (1-step up), 'l' (1-step left), 'd' (1-step down) 'r' (1-step right). You can only move to cells which have 0 value in them. You can't move out of the boundaries or in the cells which have value 1 in them (1 means obstacle)
+// 5. Complete the body of floodfill function - without changing signature - to print all paths that can be used to move from top-left to bottom-right.
+
+// Note1 -> Please check the sample input and output for details
+// Note2 -> If all four moves are available make moves in the order 't', 'l', 'd' and 'r
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        boolean[][] vis=new boolean[n][m];
+        floodfill(arr, 0, 0, "",vis);
+    }
+    
+    // asf -> answer so far
+    public static void floodfill(int[][] maze, int sr, int sc, String asf,boolean[][] vis) {
+        
+        int n=maze.length;
+        int m=maze[0].length;
+        
+        if(sr>=n || sc>=m || sr<0 || sc<0 || maze[sr][sc]==1||vis[sr][sc]==true)
+        {
+            return;
+        }
+        if(sr==n-1 && sc==m-1){
+            System.out.println(asf);
+            return;
+        }
+        vis[sr][sc]=true;
+        floodfill(maze,sr-1,sc,asf+"t",vis);
+        floodfill(maze,sr,sc-1,asf+"l",vis);
+        floodfill(maze,sr+1,sc,asf+"d",vis);
+        floodfill(maze,sr,sc+1,asf+"r",vis);
+        vis[sr][sc]=false;
+    }
+}
