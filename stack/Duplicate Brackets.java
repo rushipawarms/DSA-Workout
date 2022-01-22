4b751fc7d7329644d86c15854168f8b840b6e7ad
Rushikesh Pawar <90256959+rushipawarms@users.noreply.github.com>
2022-01-22 22:27:10 +0530

    Update Duplicate Brackets.java

--- stack/Duplicate Brackets.java
+++ stack/Duplicate Brackets.java
@@ -7,3 +7,47 @@
 // ((a + b) + (c + d)) -> false
 // (a + b) + ((c + d)) -> true
 
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn=new Scanner(System.in);
+        String str=scn.nextLine();
+        
+        
+        System.out.print(DubBr(str));
+
+    }
+    
+    public static boolean DubBr(String exp)
+    {
+        Stack<Character> stk = new Stack<>(); 
+        
+        for(int i=0;i<exp.length();i++)
+        {
+            char ch=exp.charAt(i);
+            if(ch==')')
+            {
+                if(stk.peek()=='(')
+                {
+                    return true;
+                }
+                
+                while(stk.peek()!='(')
+                {
+                    stk.pop();
+                }
+                //pop opening bracket
+                stk.pop();
+            }
+            else
+            {
+                stk.push(ch);
+            }
+        }
+        return false;
+    }
+
+}
