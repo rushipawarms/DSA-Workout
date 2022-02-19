24f28bfb4e671b6e34c0f724b238a70596f7eeb5
Rushikesh Pawar <90256959+rushipawarms@users.noreply.github.com>
2022-02-19 23:51:53 +0530

    Create Left View of Binary Tree.java

---
+++ BinaryTree/Left View of Binary Tree.java
@@ -0,0 +1,25 @@
+class Tree
+{
+    //Function to return list containing elements of left view of binary tree.
+    public static void rec(Node root, int h,ArrayList<Integer> al)
+
+    {
+        if(root==null) return ;
+        
+        if(al.size()==h)
+        {
+            al.add(root.data);
+        }
+        
+        rec(root.left,h+1,al);
+        rec(root.right,h+1,al);
+    }
+    
+    ArrayList<Integer> leftView(Node root)
+    {
+      ArrayList<Integer> al=new ArrayList<>();
+      rec(root,0,al);
+      
+      return al;
+    }
+}
