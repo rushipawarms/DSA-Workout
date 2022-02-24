bf3af8e90b6d3535080177d41b60cd40e580af35
Rushikesh Pawar <90256959+rushipawarms@users.noreply.github.com>
2022-02-24 11:23:32 +0530

    Create morrisInorderTreavalsal.java

---
+++ BinaryTree/morrisInorderTreavalsal.java
@@ -0,0 +1,40 @@
+class Solution {
+    public TreeNode rightMost(TreeNode node, TreeNode curr)
+    {
+        while(node.right!=null && node.right!=curr )
+        {
+            node=node.right;
+        }
+        return node;
+    }
+    public List<Integer> inorderTraversal(TreeNode root) 
+    {
+    
+        TreeNode curr=root;
+        ArrayList<Integer> ans=new ArrayList<>();
+        while(curr!=null)
+        {
+            TreeNode left=curr.left;
+            if(left==null)
+            {
+                ans.add(curr.val);
+                curr=curr.right;
+            }
+            else
+            {
+                TreeNode rm=rightMost(left,curr);
+                if(rm.right==null)
+                {
+                    rm.right=curr;
+                    curr=left;
+                }
+                else{
+                    rm.left=null;
+                    ans.add(curr.val);
+                    curr=curr.right;
+                }
+            }
+        }
+        return ans;
+    }
+}
